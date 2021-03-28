27efd133074bb2eb7dc2e302940abe406ebde7f0
Mkknowledge <kmayur2124@gmail.com>
2021-03-28 17:42:15 +0530

    Added HttpClientModule in appmodule

--- src/app/service/http-client.service.ts
+++ src/app/service/http-client.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export class Employee{
+  constructor(
+    public empId:string,
+    public firstname:string,
+    public lastname:string,
+    public address:string,
+    public dob: Date,
+    public mobile:string,
+    public city:string
+  ) {}
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpClientService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  getEmployees()
+  {
+    console.log("test call");
+    return this.httpClient.get<Employee[]>('http://localhost:8080/employees');
+  }
 }
